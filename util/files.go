fe134bd28c17698f73d3d36e04709c92910b3183
MewX <xiayuanzhong+gpg2020@gmail.com>
2022-12-25 18:23:03 +1100

    Added cookies helper functions

--- util/files.go
+++ util/files.go
@@ -2,9 +2,15 @@ package util
 
 import (
 	"flag"
+	"fmt"
 	"github.com/its-my-data/doubak/proto"
+	"github.com/mengzhuo/cookiestxt"
+	"html"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 const CollectorPathPrefix = "collector/"
@@ -29,4 +35,36 @@ func GetPathWithCreationWithBase(base, subdirs string) (string, error) {
 	// Recursively create sub-directories.
 	newPath := filepath.Join(base, subdirs)
 	return newPath, os.MkdirAll(newPath, os.ModePerm)
+}
+
+// LoadCookiesFile loads the external cookies file.
+func LoadCookiesFile(filePath string) ([]*http.Cookie, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	return cookiestxt.Parse(f)
+}
+
+// LoadCookiesFileToString loads the external cookies file.
+func LoadCookiesFileToString(filePath string) (string, error) {
+	cookies, err := LoadCookiesFile(filePath)
+	if err != nil {
+		return "", nil
+	}
+
+	log.Println("Loaded", len(cookies), "cookies")
+	c := make([]string, 0)
+	for _, cookie := range cookies {
+		if len(cookie.Name) == 0 || len(cookie.Value) == 0 {
+			continue
+		}
+		c = append(c, fmt.Sprintf("%s=%s", cookie.Name, html.UnescapeString(cookie.Value)))
+	}
+
+	result := strings.Join(c, "; ")
+	log.Println("Cookies are", result)
+	return result, nil
 }
